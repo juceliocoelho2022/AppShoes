0012199aa4ccac158e7e38182976ce8fc2cc23fc
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-05 07:32:27 -0300

    Camadas Creadas

--- app/src/main/java/com/devjucelio/appshoes/MainActivity.kt
+++ app/src/main/java/com/devjucelio/appshoes/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppShoesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Jucelio",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
